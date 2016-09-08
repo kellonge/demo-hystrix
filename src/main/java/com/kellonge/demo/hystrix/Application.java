d7ffa73ffbfa65101a4ab7fcdcfb5ae0460900fc
shenshuwen <1044042600@qq.com>
2016-09-08 16:53:08 +0800

    init

---
+++ src/main/java/com/kellonge/demo/hystrix/Application.java
@@ -0,0 +1,15 @@
+package com.kellonge.demo.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kellonge on 16/9/8.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
