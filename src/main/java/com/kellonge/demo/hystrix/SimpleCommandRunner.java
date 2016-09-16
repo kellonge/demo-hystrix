286f676b9a641e8516bbe73254518738f94ecb73
shenshuwen <1044042600@qq.com>
2016-09-16 17:35:12 +0800

    add 4 kind of invke

--- src/main/java/com/kellonge/demo/hystrix/SimpleCommandRunner.java
+++ src/main/java/com/kellonge/demo/hystrix/SimpleCommandRunner.java
@@ -19,27 +19,49 @@ public class SimpleCommandRunner implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        //instanceo command
-        RequestCommand command = new RequestCommand("/normal/now");
 
         //excute
         //means the result will return immediately
+        logger.info("excute");
         PrintInterval.start();
         for (int i = 0; i < 10; i++) {
-            logger.info("excute result " + command.execute());
+            logger.info("excute result " + new RequestCommand("/normal/now").execute());
         }
         PrintInterval.end();
 
         //queue
         //hystrix will put the reuslt in a `future` var, so that the request can invoke at sametime
+        logger.info("queue");
         PrintInterval.start();
         List<Future<String>> futures = Lists.newArrayList();
         for (int i = 0; i < 10; i++) {
-            Future<String> queue = command.queue();
+            Future<String> queue = new RequestCommand("/normal/now").queue();
             futures.add(queue);
         }
         for (Future<String> future : futures) {
             logger.info("excute result " + future.get());
+        }
+        PrintInterval.end();
+
+
+        //observe
+        //do request when call and handle result in hytrix thread poll
+        logger.info("observe");
+        PrintInterval.start();
+        for (int i = 0; i < 10; i++) {
+            new RequestCommand("/normal/now").observe().forEach(x -> logger.info("excute result " + x));
+        }
+        PrintInterval.end();
+
+        //make sure above request has finish
+        Thread.sleep(1000L);
+
+        //toObservable
+        //do request after call,return to main thread immediately
+        logger.info("toObservable");
+        PrintInterval.start();
+        for (int i = 0; i < 10; i++) {
+            new RequestCommand("/normal/now").toObservable().forEach(x -> logger.info("excute result " + x));
         }
         PrintInterval.end();
     }
